8f5eeecc20e34e5b681777a0b91a31fc889cbb12
Kenny <kennypluchau@gmail.com>
2023-11-02 14:38:35 -0700

    Initial

---
+++ 10-30/_0_30App.swift
@@ -0,0 +1,17 @@
+//
+//  _0_30App.swift
+//  10-30
+//
+//  Created by Kenny Luchau on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct _0_30App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
