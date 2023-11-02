12d4018001697d25280b5362db01b78066b1d539
Kenny <kennypluchau@gmail.com>
2023-11-02 15:00:35 -0700

    Is this complete or is there more?!

--- 10-30/ContentView.swift
+++ 10-30/ContentView.swift
@@ -6,18 +6,44 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @State var message = ""
+    @State var sliderValue = 0.0
+    enum Response {
+        case success
+    }
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button("Click Me") {
+                let startTime = NSDate()
+                callFunction()
+                let endTime = NSDate()
+                message = "Completed in \(endTime.timeIntervalSince(startTime as Date)) seconds."
+            }
+            Spacer()
+            Slider(value: $sliderValue)
+            Text("Message = \(message)")
         }
-        .padding()
+    }
+    func waiting() async throws -> Response {
+        Thread.sleep(forTimeInterval: 20)
+        return Response.success
+    }
+    
+    // DOESN'T THIS WORK ALREADY LIKE THIS OR IS THERE MORE?!?!?!?!?!?!?!?
+    
+    func callFunction() {
+        Task(priority: .high) {
+            do {
+                _ = try await waiting()
+            } catch {
+                
+            }
+        }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
